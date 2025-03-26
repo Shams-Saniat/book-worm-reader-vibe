3cea090bc0e5f93d31a8be611836adaeb4aadf89
Shams Saniat <51787712+Shams-Saniat@users.noreply.github.com>
2025-03-26 12:07:03 +0600

    installed React Router DOM and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center bg-green-300">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
